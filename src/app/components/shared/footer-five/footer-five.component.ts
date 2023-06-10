1cc18e6c39f5345393ecd3022aa99ea12d79ea9f
kashif <admin@kashifali.me>
2023-06-10 18:48:22 +0500

    Initial commit

---
+++ src/app/components/shared/footer-five/footer-five.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HelperService } from '../../helper/helper.service';
+
+@Component({
+  selector: 'app-footer-five',
+  templateUrl: './footer-five.component.html',
+  styleUrls: ['./footer-five.component.css']
+})
+export class FooterFiveComponent extends HelperService {
+
+  year: number = new Date().getFullYear();
+}
